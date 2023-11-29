9443168fde705bff8d56c898e4233d1bcc5dab77
UPADHYAY-YATENDRA <yupadhyayyk@gmail.com>
2023-11-29 12:26:49 +0530

    removed Proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,14 +7,14 @@ export default defineConfig({
   server: {
     port: 5000,
     // Get rid of the CORS error
-    proxy: {
-      '/api': {
-        target: 'https://threads-eu3f.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        ws: true, // Enable WebSocket proxying
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://threads-eu3f.onrender.com',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     ws: true, // Enable WebSocket proxying
+    //   },
+    // },
   },
   // Add this to specify the loader for JSX files
 
